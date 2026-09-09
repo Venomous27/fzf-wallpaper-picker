d9bd00ffef2d39a0826861c52a5b3fdabfdb0bd2
Venomous <venomousrudra27@gmail.com>
2026-09-09 22:13:37 +0530

    Update wallpaper selection to show relative paths
    Modify image selection to display relative paths in fzf.

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-IMG=$(find ~/Pictures/Wallpapers/ -type f | fzf --layout=reverse)
+IMG=$(find ~/Pictures/Wallpapers/ -type f | sed "s|$HOME/Pictures/Wallpapers/||" | fzf --layout=reverse)
 
 SET=$(awww img "$IMG" --transition-type fade --transition-duration 1 | wal -i "$IMG")
