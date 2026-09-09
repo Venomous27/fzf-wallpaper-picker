8fb31cbf27f77a1264336a387e93771cfc2bb701
Venomous <venomousrudra27@gmail.com>
2026-09-09 22:27:16 +0530

    Update wallpaper.sh to use correct image path
    Fix path handling for wallpaper selection.

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,4 +2,5 @@
 
 IMG=$(find ~/Pictures/Wallpapers/ -type f | sed "s|$HOME/Pictures/Wallpapers/||" | fzf --layout=reverse)
 
-SET=$(awww img "$IMG" --transition-type fade --transition-duration 1 | wal -i "$IMG")
+SET=$(awww img ~/Pictures/Wallpapers/$IMG --transition-type fade --transition-duration 1 | wal -i ~/Pictures/Wallpapers/$IMG)
+
