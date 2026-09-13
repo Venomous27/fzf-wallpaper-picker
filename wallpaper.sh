af18f7456839bc7799af381f5001c8682f9567e3
Venomous <venomousrudra27@gmail.com>
2026-09-13 23:29:26 +0530

    Improve wallpaper script with empty check
    Add check for empty image selection before setting wallpaper.

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,5 +2,9 @@
 
 IMG=$(find ~/Pictures/Wallpapers/ -type f | sed "s|$HOME/Pictures/Wallpapers/||" | fzf --layout=reverse)
 
-SET=$(awww img ~/Pictures/Wallpapers/$IMG --transition-type fade --transition-duration 1 | wal -i ~/Pictures/Wallpapers/$IMG)
+if [ "$IMG" == "" ]; then exit 1; fi
+
+awww img ~/Pictures/Wallpapers/$IMG --transition-type fade --transition-duration 1
+
+wal -i ~/Pictures/Wallpapers/$IMG
 
